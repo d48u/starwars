6c73abbe6a0ee6c881f92a083740f9159eb940ab
d48u <>
2018-06-25 01:12:01 +0200

    star wars test app

---
+++ resources/assets/js/components/StarWarsList.js
@@ -0,0 +1,178 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+export default class StarWarsList extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            results: [],
+            params: null
+
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://swapi.co/api/people')
+                .then(res => {
+                    return res.json();
+                })
+                .then(
+                        (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        results: result.results,
+                        params: result
+                    });
+                },
+                        (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+                )
+    }
+    
+        fetchNextPage(next) {
+        fetch(next)
+                .then(res => {
+                    return res.json();
+                })
+                .then(
+                        (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        results: result.results,
+                        params: result
+                    });
+                },
+                        (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+                )
+    }
+
+    render() {
+
+        const {error, isLoaded, results, params} = this.state;
+        if (error) {
+            return <div>Error: {error.message}</div>;
+        } else if (!isLoaded) {
+            return <div>Loading...</div>;
+        } else {
+
+            return (
+                    <div className="container">
+                        <div className="row justify-content-center">
+                            <div className="col-md-16">
+                                <div className="card">
+                                    <div className="card-header">Star Wars Characters </div>
+                    
+                                    <div className="card-body">
+                                        Star Wars Characters List
+                                    </div>
+                    
+                                    <table className="table">
+                                        <thead>
+                                            <tr>
+                                                <th>Name</th>
+                                                <th>Height</th>
+                                                <th>Mass</th>
+                                                <th>Hair color</th>
+                                                <th>Skin color</th>
+                                                <th>Eye color</th>
+                                                <th>Birth year</th>
+                                                <th>Gender</th>
+                                                <th>Homeworld</th>
+                                                <th>Films</th>
+                                                <th>Species</th>
+                                                <th>Vehicles</th>
+                                                <th>Starships</th>
+                                                <th>Created</th>
+                                                <th>Edited</th>
+                                                <th>Url</th>
+                                            </tr>
+                                        </thead>
+                    
+                                        <tbody>
+                    
+                                            {results.map(item => (
+                                                        <Character result={ item } />
+                                                                ))}
+                    
+                                        {/* 
+                                         <ul>
+                                         <li>
+                                         { this.state.params.next }
+                                         </li>
+                                         </ul>
+                                         */} 
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    );
+        }
+    }
+
+}
+
+function Character(props) {
+    return (
+            <tr>
+                <td>{ props.result.name }</td>
+                <td>{ props.result.height }</td>
+                <td>{ props.result.mass }</td>
+                <td>{ props.result.hair_color }</td>
+                <td>{ props.result.skin_color }</td>
+                <td>{ props.result.eye_color }</td>
+                <td>{ props.result.birth_year }</td>
+                <td>{ props.result.gender }</td>
+                <td><a href={ props.result.homeworld }>A</a></td>       
+                <td>                                                  
+                    {props.result.films.map(url => (
+                                        <UrlApi url={ url } />
+                                    ))}
+            </td>
+            <td>
+                {props.result.species.map(url => (
+                                <UrlApi url={ url } />
+                            ))}
+            </td>
+            <td>
+                {props.result.vehicles.map(url => (
+                                <UrlApi url={ url } />
+                            ))}
+            </td>
+            <td>
+                {props.result.starships.map(url => (
+                                <UrlApi url={ url } />
+                            ))} 
+            </td>
+            <td>{ props.result.created }</td>  
+            <td>{ props.result.edited }</td>
+            <td><a href={ props.result.url }>A</a></td>  
+            </tr>
+
+            );
+}
+
+function UrlApi(props) {
+    return (
+            <a href={ props.url }>A</a>
+            );
+}
+
+
+if (document.getElementById('starwarslist')) {
+    ReactDOM.render(<StarWarsList />, document.getElementById('starwarslist'));
+}
---
+++ routes/api.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+/**
+** Basic Routes for a RESTful service:
+**
+** Route::get($uri, $callback);
+** Route::post($uri, $callback);
+** Route::put($uri, $callback);
+** Route::delete($uri, $callback);
+**
+**/
+
+//Route::get('products', function () {
+//    return response(['Product 1', 'Product 2', 'Product 3'],200);
+//});
